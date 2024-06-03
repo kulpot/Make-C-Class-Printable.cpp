1faf7d4c8a2ef513de82cb1753c9d5255ad1c522
Kulpot <kulpotwahu@gmail.com>
2024-06-04 02:36:29 +0800

    Main.cpp->
    #include <iostream>
    #include <string>
    #include "Rectangle.h"
    
    //Make C++ Class Printable | C++ Tutorial  //ref link:https://www.youtube.com/watch?v=k2UZRwAOv6s&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=5
    
    int main() {
     int x = 13;
     double y = 3.14159;
     bool b = false;
     std::string str = "C++";
    
     return 0;
    }

---
+++ Make C++ Class Printable/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Rectangle.h"
+
+//Make C++ Class Printable | C++ Tutorial  //ref link:https://www.youtube.com/watch?v=k2UZRwAOv6s&list=PLFk1_lkqT8MaImkuU0ArJ6_dbr9nEN46a&index=5
+
+int main() {
+	int x = 13;
+	double y = 3.14159;
+	bool b = false;
+	std::string str = "C++";
+
+	return 0;
+}
